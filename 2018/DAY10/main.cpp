42b81bffc5e1202fd1792cf2fc424936f0523960
yhytoto12 <31928395+yhytoto12@users.noreply.github.com>
2019-12-29 22:34:59 +0900

    Update main.cpp

--- 2018/DAY10/main.cpp
+++ 2018/DAY10/main.cpp
@@ -1,5 +1,22 @@
 #include <bits/stdc++.h>
 
+void part12(std::vector<std::pair<int, int>> &pos, std::vector<std::pair<int, int>> &vel, int t) {
+    int N = pos.size();
+    const int MAXGRID = 350;
+    char A[MAXGRID][MAXGRID] = {' '};
+    for(int k = 0; k < N; ++k) {
+        int x = pos[k].first + vel[k].first * t;
+        int y = pos[k].second + vel[k].second * t;
+        if(x >= 0 && x < MAXGRID && y >= 0 && y < MAXGRID) A[x][y] = '#';
+    }
+    std::cout << t << " time after : \n";
+    for(int i = 0; i < MAXGRID; ++i) {
+        for(int j = 0; j < MAXGRID; ++j) {
+            std::cout << A[j][i];
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main() {
 	// read input
@@ -10,13 +27,8 @@ int main() {
 		pos.push_back({stoi(str.substr(10, 6)), stoi(str.substr(18, 6))});
 		vel.push_back({stoi(str.substr(36, 2)), stoi(str.substr(40, 2))});
 	}
-	int N = pos.size();
-	const int OFFSET = 10000;
-	for(int i = 0; i < N; ++i) {
-		pos[i].first += vel[i].first * OFFSET;
-		pos[i].second += vel[i].second * OFFSET;
-	}
-	for(auto &p : pos) { 
-		std::cout << p.first << " " << p.second << "\n";
-	}
+	
+	// part1
+	std::cout << "=========== PART 1 & 2 ===========\n";
+	part12(pos, vel, 10054);
 }
